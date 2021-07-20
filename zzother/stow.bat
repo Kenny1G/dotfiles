ae9006ed4f2b8d47086dccc529c102c1cd0a1aa5
Kenny Osele <kennyosele@microsoft.com>
2021-07-20 14:28:06 -0700

    added bat file to automatically fetch vim settings for windows

---
+++ zzother/stow.bat
@@ -0,0 +1,5 @@
+mklink %userprofile%\AppData\Local\nvim\init.vim %userprofile%\dotfiles\nvim\.config\nvim\init.vim
+mklink %userprofile%\.vimrc %userprofile%\dotfiles\vim\.vimrc
+mklink /J %userprofile%\.vimcfg %userprofile%\dotfiles\vim\.vimcfg
+pause
+
